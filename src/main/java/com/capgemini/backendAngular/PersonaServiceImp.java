535ad7f90347de39530313be84b003f3463cb270
diego <diego.cortinagarcia@gmail.com>
2022-05-27 09:56:27 +0200

    added interface

---
+++ src/main/java/com/capgemini/backendAngular/PersonaServiceImp.java
@@ -0,0 +1,37 @@
+package com.capgemini.backendAngular;
+
+import java.util.List;
+
+public class PersonaServiceImp implements PersonaService {
+
+	@Override
+	public List<Persona> listar() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Persona listarId(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Persona edit(Persona p) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Persona add(Persona p) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(int id) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
