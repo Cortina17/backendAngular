080683dd4d5fdc4735d1253c60caa911f821e127
diego <diego.cortinagarcia@gmail.com>
2022-05-27 10:44:13 +0200

    working

--- src/main/java/com/capgemini/backendAngular/PersonaServiceImp.java
+++ src/main/java/com/capgemini/backendAngular/PersonaServiceImp.java
@@ -3,7 +3,9 @@ package com.capgemini.backendAngular;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PersonaServiceImp implements PersonaService {
 
 	@Autowired
