b5c805a7db6fdaab6a1842204e396a4efa37c506
diego <diego.cortinagarcia@gmail.com>
2022-05-27 09:33:50 +0200

    dd

--- src/main/java/com/capgemini/backendAngular/Persona.java
+++ src/main/java/com/capgemini/backendAngular/Persona.java
@@ -1,10 +1,20 @@
 package com.capgemini.backendAngular;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "personas")
 public class Persona {
+	
+	@Column
+	private int id;
+	
+	@Column
+	private String nombre;
+	
+	@Column
+	private String apellido;
 
 }
