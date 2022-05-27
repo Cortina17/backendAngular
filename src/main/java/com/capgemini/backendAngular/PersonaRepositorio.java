4e2674fc005c808b1cf53a40821f81f674061800
diego <diego.cortinagarcia@gmail.com>
2022-05-27 09:45:41 +0200

    crud persona repo

--- src/main/java/com/capgemini/backendAngular/PersonaRepositorio.java
+++ src/main/java/com/capgemini/backendAngular/PersonaRepositorio.java
@@ -1,9 +1,18 @@
 package com.capgemini.backendAngular;
 
+import java.util.List;
 import org.springframework.data.repository.Repository;
 
 public interface PersonaRepositorio extends Repository<Persona, Integer> {
-
+	// dialogar con el motor de MySQL
+	// CRUD
 	
-}
+	List<Persona> findAll(); // overriding el findAll del repository
 
+	Persona findById(int id);
+
+	Persona save(Persona p);
+
+	void delete(Persona p);
+
+}
