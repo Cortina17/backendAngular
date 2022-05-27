df5e5c9faa1dc02fa6debe6cc9d3356b2fde041e
diego <diego.cortinagarcia@gmail.com>
2022-05-27 09:41:33 +0200

    Repo

---
+++ src/main/java/com/capgemini/backendAngular/PersonaRepositorio.java
@@ -0,0 +1,9 @@
+package com.capgemini.backendAngular;
+
+import org.springframework.data.repository.Repository;
+
+public interface PersonaRepositorio extends Repository<Persona, Integer> {
+
+	
+}
+
